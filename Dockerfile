a7e2ac2f5c66b6a91f3872d21585d44007a0d7b7
Julian Nonino <noninojulian@gmail.com>
2023-01-19 13:41:46 -0300

    Update docker image

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
-FROM python:3.11.1-alpine
+FROM alpine:3.17.1
 LABEL maintainer="Julian Nonino <noninojulian@gmail.com>"
 
-# Install dependencies
-RUN apk add ---no-cache --update groff less && \
-    rm -rf /var/cache/apk/*
+# renovate: datasource=github-tags depName=aws/aws-cli
+ARG AWS_CLI_VERSION=2.9.16
 
 # Install dependencies
-COPY requirements.txt .
-RUN pip install --no-cache-dir --requirement requirements.txt && \
-    rm -rf ~/.cache/pip
+RUN apk add --no-cache --update bash groff less && \
+    rm -rf /var/cache/apk/*
+
+# Install AWS Cli version 2
+COPY install_awscli.sh /tmp/install_awscli.sh
+RUN /tmp/install_awscli.sh && \
+    rm /tmp/install_awscli.sh
 
 # Setup entrypoint
 COPY entrypoint.sh /usr/local/bin
---
+++ install_awscli.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a nonzero exit value.
+set -e
+
+echo "========================================="
+echo "Building and installing AWS Cli version 2"
+echo "========================================="
+
+echo "--> Install AWS Cli build dependencies as a virtual package"
+apk add --no-cache --virtual .aws-cli-build-deps git build-base libffi-dev cmake python3 py3-pip python3-dev gcompat
+
+echo "--> Clone the AWS Cli repository"
+git clone --single-branch --depth 1 -b ${AWS_CLI_VERSION} https://github.com/aws/aws-cli.git
+
+echo "--> cd into the cloned repository"
+cd aws-cli
+
+echo "--> Start a Pyhon virtual environment"
+python -m venv venv
+. venv/bin/activate
+
+echo "--> Build AWS Cli"
+scripts/installers/make-exe
+
+echo "--> Install AWS Cli"
+unzip -q dist/awscli-exe.zip
+aws/install
+
+echo "--> Remove cloned repository"
+rm -rf aws-cli
+
+echo "--> Delete virtual package with AWS Cli build dependencies"
+apk del .aws-cli-build-deps
